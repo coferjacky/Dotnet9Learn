2b215d9a1e6738779c138605f08cf9dea5cbd849
jackbaul <jackbaul@189.cn>
2026-01-28 18:32:39 +0800

    audoEvents

--- 174AutoImplementedEvents/Program.cs
+++ 174AutoImplementedEvents/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using _174AutoImplementedEvents;
+
 Console.WriteLine("Hello, World!1");
-//自动实现事件的目标是 
+//自动实现事件的目标是 简化事件声明的语法，从而减少样板代码。
+
+//创建发布者对象
+Publisher pub = new Publisher();
+//创建订阅者对象   
+SubScriber sub = new SubScriber();
+
+//订阅
+pub.MyEvent += sub.Add;
+pub.MyEvent += sub.Multi;
+
+//触发事件,使得两个方法执行
+pub.RaiseEvent(10, 20);
---
+++ 174AutoImplementedEvents/SubScriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _174AutoImplementedEvents
+{
+
+
+    internal class SubScriber
+    {//订阅者类
+        public void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+
+        public void Multi(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+    }
+
+
+}
