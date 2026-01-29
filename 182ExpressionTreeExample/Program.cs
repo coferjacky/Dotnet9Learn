0e647be3d7699fc484f7a2388e3d0c461a211612
Jackbaul <22878236@qq.com>
2026-01-29 21:52:46 +0800

    表达式树

--- 182ExpressionTreeExample/Program.cs
+++ 182ExpressionTreeExample/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
 Console.WriteLine("Hello, World!");
+
+Expression<Func<int,int>> e=x=>x*x;
+Func<int,int> mydelegates=e.Compile();
+
+
+
+Console.WriteLine(mydelegates.Invoke(10)); 
