62de12c039ddf77b8b02afcfd1a9a35800b235a5
Jackbaul <22878236@qq.com>
2026-02-10 22:29:42 +0800

    IEnumerable 更新

---
+++ IEnumerableExample/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+IEnumerable<string> list = new List<string>() { "how are you","how old you "};
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+//模拟IEnumerator如何实现
+Console.WriteLine("模拟IEnumerator如何实现");
+IEnumerator<string> et = list.GetEnumerator(); 
+//将指针reset
+et.Reset();
+et.MoveNext();
+Console.WriteLine(et.Current);
+
