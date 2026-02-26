27b23b6d056d951257118054f9c44879003bc585
jackbaul <jackbaul@189.cn>
2026-02-26 18:33:01 +0800

    Semaphore.Mehtod

---
+++ 424SemaphoreDemo/Shared.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _424SemaphoreDemo
+{
+    public class Shared
+    {
+        public static object LockObject {  get; set; }  
+        public static string FilePath {  get; set; }
+        public static int ChunkSize { get; set; }   
+
+        public static int MaxConcurrency { get; set; }
+        static Shared()
+        {
+            LockObject = new object();
+            FilePath = "data.csv";
+            ChunkSize = 100;
+        }
+    }
+}
