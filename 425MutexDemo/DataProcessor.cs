8f0945565a1dc08c180f56390cd487ace5e9db03
Jackbaul <22878236@qq.com>
2026-02-26 23:25:04 +0800

    Mutex

---
+++ 425MutexDemo/DataProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _425MutexDemo
+{
+    internal class DataProcessor
+    {
+        public string ChunkName { get; set; }
+        public List<string> Chunk { get; set; }
+
+        public Dictionary<string, int> GenderCounts=new Dictionary<string, int>();
+        public void ProcessChunk()
+        {
+            //1,Tabbitha,Warrington,twarrington0@a8.net,Female
+            foreach (string line in Chunk)
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+                string[] values=line.Split(",");
+                if (values.Length >= 5) {
+                  string gender=  values[4].Trim().ToLower();
+                  if (GenderCounts.ContainsKey(gender))
+                  {
+                        GenderCounts[gender]++;
+                  }
+                  else
+                  {
+                        GenderCounts.Add(gender,1);
+                  }
+                }
+                //模拟人为延迟
+                Thread.Sleep(100 * new Random().Next(2, 5));
+            }
+            
+            
+        }
+    }
+}
---
+++ 425MutexDemo/Shared.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _425MutexDemo
+{
+    public class Shared
+    {
+        public static Mutex mutex {  get; set; }  
+        public static string FilePath {  get; set; }
+        public static int ChunkSize { get; set; }   
+
+        public static int MaxConcurrency { get; set; }
+
+
+        static Shared()
+        {
+            mutex = new Mutex();
+            FilePath = "data.csv";
+            ChunkSize = 100;
+        }
+    }
+}
