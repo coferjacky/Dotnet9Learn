3f14a64141751b82fdf5dde858a6a2078799c5a7
jackbaul <jackbaul@189.cn>
2026-02-28 18:32:21 +0800

    CountDown

---
+++ 427CountDownEvent/Shared.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _427CountDownEvent
+{
+    public class Shared
+    {
+        public static Mutex mutex {  get; set; }  
+        public static string FilePath {  get; set; }
+        public static int ChunkSize { get; set; }   
+
+        public static int MaxConcurrency { get; set; }
+
+        public static CountdownEvent Count{ get; set; }
+
+
+        static Shared()
+        {
+            mutex = new Mutex();
+            FilePath = "data.csv";
+            ChunkSize = 100;
+            MaxConcurrency = 3;
+        }
+    }
+}
