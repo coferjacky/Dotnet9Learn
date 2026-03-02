e8aa4aea43f0aa41ec005f737b9a3da01130ed51
cofer <22878236@qq.com>
2026-03-03 00:12:45 +0800

    Task

---
+++ 434TaskExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Headers;
+
+namespace _434TaskExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UpCounter upCount = new UpCounter();
+            DownCounter downCount=new DownCounter();
+
+
+            //下面不要这样创建Task对象
+            //Task task = new Task();
+            Task task1 = Task.Run(() => {             
+                upCount.CountUp(20);
+            });
+
+            Task task2 = Task.Run(() => {
+                downCount.CountDown(20);
+            });
+
+            //上面代码Task.Run()方法会立即执行委托中的代码，并返回一个Task对象并自动启动它，你省去了调用Start()方法的麻烦。
+            Console.ReadKey();
+        }
+    }
+    class UpCounter
+    {
+        public void CountUp(int count)
+        {
+            Console.WriteLine("\nCount-up start");
+            for (int i = 1; i < count; i++)
+            {
+                Console.Write($"i={i}, ");
+            }
+            Console.WriteLine("\nCount-up end");
+        }
+    }
+
+    class DownCounter
+    {
+        public void CountDown(int count)
+        {
+            Console.WriteLine("\nCount-down start");
+            for (int i = count; i>= 1; i--)
+            {
+                Console.Write($"j={i}, ");
+            }
+            Console.WriteLine("\nCount-down end");
+        }
+    }
+}
