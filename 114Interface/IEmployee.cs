63f659bf40097e90751109f14b23784eab2f4a18
cofer <22878236@qq.com>
2026-09-16 00:26:14 +0800

    interface demo

---
+++ 114Interface/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _114Interface
+{
+    internal interface IEmployee
+    {
+        string GetHealthInsuranceAmount();
+        int EmpID {  get; set; }
+        string EmpName { get; set; }
+        string Location {  get; set; }
+    }
+}
---
+++ 114Interface/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _114Interface;
+
+Console.WriteLine("Hello, World!");
+
+
+public class Manager : IEmployee
+{
+    private string _region;
+    private int _id;
+    private string _name;
+    private string _location;
+    public int EmpID { get => _id; set => _id=value; }
+    public string EmpName { get => _name; set => _name=value; }
+    public string Location { get =>_location; set => _location=value; }
+
+    public string GetHealthInsuranceAmount()
+    {
+        return "1000";
+    }
+}
